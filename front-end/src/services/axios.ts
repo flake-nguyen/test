b41e6b7e46cfe101e66f1de3b8dc7221d2e3dae6
flake-nguyen <21021262@vnu.edu.vn>
2025-06-29 11:55:57 +0700

    Update axios.ts

--- front-end/src/services/axios.ts
+++ front-end/src/services/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://test-back-end-9h00.onrender.com",
   timeout: 5000,
 });
 
